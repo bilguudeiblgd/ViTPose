870e6ffb834e224b8bc80c7770002f9bae7fd0cd
Bilguudei Baljinnyam <bilguudei.b8@gmail.com>
2024-04-06 13:48:01 +0200

    small changes to gitignore

--- configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/mpii/ViTPose_base_mpii_256x192.py
+++ configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/mpii/ViTPose_base_mpii_256x192.py
@@ -118,10 +118,10 @@ test_pipeline = val_pipeline
 
 data_root = '/datagrid/personal/baljibil/data/MPII_COCO'
 data = dict(
-    samples_per_gpu=32,
+    samples_per_gpu=64,
     workers_per_gpu=2,
-    val_dataloader=dict(samples_per_gpu=16),
-    test_dataloader=dict(samples_per_gpu=16),
+    val_dataloader=dict(samples_per_gpu=32),
+    test_dataloader=dict(samples_per_gpu=32),
     train=dict(
         type='TopDownMpiiDataset',
         ann_file=f'{data_root}/annotations/mpii_train.json',
